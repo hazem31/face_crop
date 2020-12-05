0098f6f9e04be4bbd7bae83732a2195926aa3645
hazem <hazemnashar31@gmail.com>
2020-12-05 15:53:12 +0200

    Add files via upload

---
+++ cnn-face-detector-dlib.py
@@ -0,0 +1,123 @@
+# import required packages
+import cv2
+import dlib
+import argparse
+import time
+import numpy as np
+# handle command line arguments
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True, help='path to image file')
+ap.add_argument('-w', '--weights', default='./mmod_human_face_detector.dat',
+                help='path to weights file')
+args = ap.parse_args()
+
+# load input image
+image = cv2.imread(args.image)
+
+if image is None:
+    print("Could not read input image")
+    exit()
+    
+# initialize hog + svm based face detector
+hog_face_detector = dlib.get_frontal_face_detector()
+
+# initialize cnn based face detector with the weights
+cnn_face_detector = dlib.cnn_face_detection_model_v1(args.weights)
+
+start = time.time()
+
+# apply face detection (hog)
+faces_hog = hog_face_detector(image, 1)
+
+end = time.time()
+print("Execution Time (in seconds) :")
+print("HOG : ", format(end - start, '.2f'))
+
+# loop over detected faces
+for face in faces_hog:
+    x = face.left()
+    y = face.top()
+    w = face.right() - x
+    h = face.bottom() - y
+
+    # draw box over face
+    cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 2)
+
+
+start = time.time()
+
+# apply face detection (cnn)
+faces_cnn = cnn_face_detector(image, 1)
+
+end = time.time()
+print("CNN : ", format(end - start, '.2f'))
+
+# loop over detected faces
+for face in faces_cnn:
+    x = face.rect.left()
+    y = face.rect.top()
+    w = face.rect.right() - x
+    h = face.rect.bottom() - y
+
+     # draw box over face
+    cv2.rectangle(image, (x,y), (x+w,y+h), (0,0,255), 2)
+
+
+
+
+# write at the top left corner of the image
+# for color identification
+img_height, img_width = image.shape[:2]
+cv2.putText(image, "HOG", (img_width-50,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                (0,255,0), 2)
+cv2.putText(image, "CNN", (img_width-50,40), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                (0,0,255), 2)
+
+# display output image
+cv2.imshow("face detection with dlib", image)
+cv2.waitKey()
+
+# save output image 
+cv2.imwrite("cnn_face_detection.png", image)
+
+# close all windows
+cv2.destroyAllWindows()
+
+
+img = cv2.imread('input3.jpg')
+
+
+
+
+def detect(frame):
+    """
+    
+    THIS FUNCTION DETECTS THE FACE IN GREY IMAGE AND CROP IT THEN PREDICT ITS  FACE FEATURE
+    
+    PAPAMETER:
+    GREY:  np array; THE GREY SCALE OF IMAGE
+    FRAME: np array; ACTUAL IMAGE
+    
+    RETURNS:
+    FACE_FEATURE: np.array; PREDICTED ARRAY OF SIZE (1,2048)
+    FRAME_CROP: np array; CROPED IMAGE
+    
+    """
+    faces_cnn = cnn_face_detector(frame, 1)
+    for face in faces_cnn:
+        x = face.rect.left()
+        y = face.rect.top()
+        w = face.rect.right() - x
+        h = face.rect.bottom() - y
+    frame = frame[y:y+h,x:x + w]
+    print(frame.shape)
+    frame =  cv2.resize(frame, (224,224))
+    print(frame.shape)
+    #frame = np.array(frame.resize((224,224)).reshape((1,224,224,3)))
+    #frame = np.expand_dims(frame, axis=0)
+    return frame
+
+
+
+frame = detect(img)
+cv2.imwrite('out.jpg', frame)
